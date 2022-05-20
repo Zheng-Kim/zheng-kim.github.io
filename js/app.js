93c48cc7f2d7dc4366472c5a92f2faa58d79925c
Kien <69453077+hwcheng32@users.noreply.github.com>
2022-05-20 16:56:06 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// console.log("目前正在學習javascript喔");
+// console.log("目前正在學習java喔");
+// alert("歡迎進入js文件");
+// let x = prompt("請輸入您的姓名");
+// alert("歡迎您" + x);
+// console.log(x);
+// alert("a");
+// // let num = 3.15945949;
+// // num = num.toFixed(3);
+// // alert(num);
+
+// let str = "My name is AAAA";
+// console.log(str.split("A"));
