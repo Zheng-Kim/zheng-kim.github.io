c4dcd753da865f46b0df816f71c620cab5e84fda
Kien <69453077+hwcheng32@users.noreply.github.com>
2022-05-24 16:30:04 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,13 +1,55 @@
-// console.log("目前正在學習javascript喔");
-// console.log("目前正在學習java喔");
-// alert("歡迎進入js文件");
-// let x = prompt("請輸入您的姓名");
-// alert("歡迎您" + x);
-// console.log(x);
-// alert("a");
-// // let num = 3.15945949;
-// // num = num.toFixed(3);
-// // alert(num);
+// // // // console.log("目前正在學習javascript喔");
+// // // // console.log("目前正在學習java喔");
+// // // // alert("歡迎進入js文件");
+// // // // let x = prompt("請輸入您的姓名");
+// // // // alert("歡迎您" + x);
+// // // // console.log(x);
+// // // // alert("a");
+// // // // // let num = 3.15945949;
+// // // // // num = num.toFixed(3);
+// // // // // alert(num);
 
-// let str = "My name is AAAA";
-// console.log(str.split("A"));
+// // // // let str = "My name is AAAA";
+// // // // console.log(str.split("A"));
+// // // console.log(4 != "4"); // false
+// // // console.log(4 !== "4"); // true
+
+// // // // let x = true, y = true;
+// // // // if (x && y) {
+// // // //     console.log("yes");
+// // // // }
+// // // // else {
+// // // //     console.log("no");
+// // // // }
+// // // let x = true;
+// // // if (x ) {
+// // //     console.log("yes");
+// // // }
+// // // else {
+// // //     console.log("no");
+// // // }
+
+// // // console.log(typeof x)
+// // let friends = ["Amy", "Kevin", "Cathy"];
+
+// // function addFriend(name,age) {
+
+// // 	console.log("新增一位朋友");
+// // 	console.log("歡迎新朋友" + name +"，他本身已經"+age+"歲");
+// // 	friends.push(name);
+// // }
+
+// // addFriend("Lebron",17); //呼叫函式
+// // console.log(friends);
+
+// function celToFah(cel) { 
+// 	let cel = prompt("請輸入攝氏溫度");
+
+// 	let fah = cel * 1.8 + 32;
+
+// 	return fah;
+
+// }
+// let input = prompt("請輸入攝氏溫度");
+// let output=celToFah(input);
+// alert("華氏溫度為" + output);
